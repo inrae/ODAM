d09461ebfa8a04bbc9674f9efc6284ddd1d080f6
djacob65@gmail.com <djacob65@gmail.com>
2017-06-21 12:49:16 +0200

    update

--- dataexplorer/global.R
+++
@@ -1,8 +0,0 @@
-options(width=128)
-options(shiny.deprecation.messages=FALSE)
-options(shiny.sanitize.errors = FALSE)
-
-externalURL <- 'http://gaia/getdata/'
-internalURL <- ''
-
-idVersion <- '1.1'

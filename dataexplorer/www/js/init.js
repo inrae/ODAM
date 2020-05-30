42f2aae8df874b214931663a8ff3e97b8adc8b63
djacob65@gmail.com <djacob65@gmail.com>
2020-05-30 10:29:27 +0000

    Deployed  with MkDocs version: 1.1

--- dataexplorer/www/js/init.js
+++
@@ -1,5 +0,0 @@
-/*! highlight.js v9.10.0 | BSD3 License | git.io/hljslicense */
-$(document).on("shiny:value", function(event) {
-  if (event.target.id === "sessioninfo") { hljs.highlightBlock( document.getElementById("sessioninfo" ) ); }
-});
-hljs.initHighlightingOnLoad();

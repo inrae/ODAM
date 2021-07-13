351c71bd6b519bc315a0d9f39b789ad3d41108a3
djacob65@gmail.com <djacob65@gmail.com>
2021-07-13 15:16:20 +0200

    update

---
+++ dataexplorer/global.R
@@ -0,0 +1,27 @@
+library(shiny)
+library(shinydashboard)
+library(shinyjs)
+library(shinyBS)
+library(networkD3)
+library(plotly)
+library(markdown)
+
+options(width=128)
+options(shiny.deprecation.messages=FALSE)
+options(shiny.sanitize.errors = FALSE)
+
+source("Rsrc/utils.R")
+conffile <- "conf/global.ini"
+conf <- Parse.INI(conffile, section="GLOBAL")
+
+idVersion <- conf$VERSION
+
+# API URL
+externalURL <- conf$GETDATA_URL_PROXY
+
+# determines whether curl verifies the authenticity of the peer's certificate
+SSL_VerifyPeer <- conf$SSL_VERIFYPEER
+
+# Maximum number of variables in a data subset so that it  can be explored interactively
+maxVariables <- conf$MAXVARIABLES
+
